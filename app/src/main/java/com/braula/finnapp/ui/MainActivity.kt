5a66b2a670d315a7c2838e84295d982067fe1f34
Aleksander Braula <aleksander.braula@abax.com>
2023-06-13 18:24:30 +0200

    :building_construction: added basic navigation

--- app/src/main/java/com/braula/finnapp/ui/MainActivity.kt
+++ app/src/main/java/com/braula/finnapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.braula.finnapp
+package com.braula.finnapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.braula.finnapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
